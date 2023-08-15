8a53c993cbb4ee01dd16e8eb3c9da0b392677f4a
Antontsev Dmitry <antontsev.dmitry@yandex.ru>
2023-08-15 22:08:56 +0300

    Version with complex solutions

--- SupportingFunc.cpp
+++ SupportingFunc.cpp
@@ -8,12 +8,17 @@
 void Solving(double a, double b, double c)
 {
 	double D;
+
 	if ((a > 0) || (a < 0))
 	{
 		D = (b * b) - (4 * a * c);
 		if (D < 0)
 		{
-			printf("Equation has not got real solution\n");
+			printf("Equation has not got real solution\n"
+					"Complex solutions:\n"
+					"Real part   Imaginary part\n");
+			printf("%-11.2lf %.2lf\n", (-b)/(2*a), sqrt(-D)/(2*a));
+			printf("%-11.2lf %.2lf\n", (-b)/(2*a), -(sqrt(-D))/(2*a));
 		}
 		else if (D > 0)
 		{
@@ -27,11 +32,17 @@ void Solving(double a, double b, double c)
 			printf("Solution is: %.2lf\n", ((-b) + sqrt(D)) / (2 * a));
 		}
 	}
-	else
+
+	else if ((b > 0) || (b < 0))
 	{
 		printf("This is not quadratic equation\n"
-				"But solution is: %.2lf\n", (-c)/b);
+				"But solution of linear equation is: %.2lf\n", (-c)/b);
+	}
 
+	else
+	{
+		printf("This is not equation:\n"
+				"a and b ratios mustn't be 0\n");
 	}
 }
 
--- main.cpp
+++ main.cpp
@@ -6,18 +6,17 @@
 int main(void) 
 {
 	double a, b, c;      // ratios of quadratic equation
-	
+
 	printf("Enter a b c ratios of quadratic equation\n");
 	while (1)
 	{
 		if (scanf("%lf %lf %lf", &a, &b, &c) != 3)
-		{
 			printf("Please enter digital data\n");
-			ScipInput();
-		}
 		else
 			break;
+		ScipInput();
 	}
+		Solving(a, b, c);
 
-	Solving(a, b, c);
+	printf("Programm done!\n");
 }
