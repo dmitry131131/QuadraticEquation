0949acf41adc63913831f3cc1a602c5ad96fbd47
Antontsev Dmitry <antontsev.dmitry@yandex.ru>
2023-08-15 23:35:29 +0300

    Fixed warning with comparison of double variables

--- ComputationalFunc.cpp
+++ ComputationalFunc.cpp
@@ -8,7 +8,7 @@ void SolvingQuadraticEquation(double a, double b, double c)
 {
     double D;     // Discriminant
 
-    if ((a > 0) || (a < 0))
+    if (fabs(a - 0) > 0.000001)
     {
         D = (b * b) - (4 * a * c);
         if (D < 0)
@@ -16,26 +16,26 @@ void SolvingQuadraticEquation(double a, double b, double c)
             printf("Equation has not got real solution\n"
                     "Complex solutions:\n"
                     "Real part   Imaginary part\n");
-            printf("%-11.2lf %.2lf\n", (-b)/(2*a), sqrt(-D)/(2*a));
-            printf("%-11.2lf %.2lf\n", (-b)/(2*a), -(sqrt(-D))/(2*a));
+            printf("%-11.4lf %.4lf\n", (-b)/(2*a), sqrt(-D)/(2*a));
+            printf("%-11.4lf %.4lf\n", (-b)/(2*a), -(sqrt(-D))/(2*a));
         }
         else if (D > 0)
         {
             printf("Equation has two real solution\n");
-            printf("First solution: %.2lf\n", (-b + (sqrt(D))) / (2 * a));
-            printf("Second solution: %.2lf\n", (-b - (sqrt(D))) / (2 * a));
+            printf("First solution: %.4lf\n", (-b + (sqrt(D))) / (2 * a));
+            printf("Second solution: %.4lf\n", (-b - (sqrt(D))) / (2 * a));
         }
         else
         {
             printf("Equation has one real solution\n");
-            printf("Solution is: %.2lf\n", ((-b) + sqrt(D)) / (2 * a));
+            printf("Solution is: %.4lf\n", ((-b) + sqrt(D)) / (2 * a));
         }
     }
 
-    else if ((b > 0) || (b < 0))
+    else if (fabs(b - 0) > 0.000001)
     {
         printf("This is not quadratic equation\n"
-                "But solution of linear equation is: %.2lf\n", (-c)/b);
+                "But solution of linear equation is: %.4lf\n", (-c)/b);
     }
 
     else
